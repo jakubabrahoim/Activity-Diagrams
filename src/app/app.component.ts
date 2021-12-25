2bdce9cfe3a0ddc319890e563d0a2fef2976b276
Jakub <jakub.abrahoim@windowslive.com>
2021-12-25 20:49:52 +0100

    jointjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+//declare var $:JQueryStatic;
+import * as jQuery from 'jquery';
+import * as _ from 'lodash';
+import * as backbone from 'backbone';
+import * as joint from 'jointjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-test';
+
+  public ngOnInit(): void {
+    let namespace = joint.shapes;
+
+    let graph = new joint.dia.Graph({}, {cellNamespace: namespace});
+
+    let paper = new joint.dia.Paper({
+      el: jQuery("#paper"),
+      width: 600,
+      height: 200,
+      model: graph,
+      gridSize: 1
+    });
+
+    let rect = new joint.shapes.basic.Rect({
+      position: { x: 100, y: 30 },
+      size: { width: 100, height: 30 },
+      attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+    });
+
+    let rect2 = rect.clone() as joint.shapes.basic.Rect;
+    rect2.translate(300);
+
+    var link = new joint.dia.Link({
+      source: { id: rect.id },
+      target: { id: rect2.id }
+    });
+
+    graph.addCells([rect, rect2, link]);
+  }
 }
