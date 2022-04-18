bfebe9e43d1a148575fd4fd1c3eff4644cf6d0d3
jakubabrahoim <jakub.abrahoim@windowslive.com>
2022-04-18 17:08:21 +0200

    Added link routing + minor updates

--- src/app/activity-diagrams/other-classes/action.ts
+++ src/app/activity-diagrams/other-classes/action.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import * as joint from 'jointjs';
-import { ElementPort } from './port';
-
 @Injectable()
 export class ActionElement {
 
@@ -12,15 +10,7 @@ export class ActionElement {
             name: 'action',
             attrs: { text: { text:  'test action' }, body: {rx: 10, ry: 10} } 
         });
-
-        //var elementPort = new ElementPort();
-
-        // -8, -8 - left port
-        //var port = elementPort.createPort(-8, -8);
-        //actionElement.addPort(port);
-        //
-        //port = elementPort.createPort(8, -8);
-
+        
         return actionElement;
     }
 }
--- src/app/activity-diagrams/other-classes/paper.ts
+++ src/app/activity-diagrams/other-classes/paper.ts
@@ -4,10 +4,10 @@ import * as joint from 'jointjs';
 @Injectable()
 export class Paper {
     
-  // funckia, ktora vytvara novy 'papier' s parametrami
+  /** Creates new JointJS paper */
   public createNewPaper(paperElement: any, graph: any, width: number, height: number, nameSpace: any): joint.dia.Paper{
       
-    // vysvetlenie parametrov https://resources.jointjs.com/docs/jointjs/v3.5/joint.html#dia.Paper.prototype.options 
+    // https://resources.jointjs.com/docs/jointjs/v3.5/joint.html#dia.Paper.prototype.options 
     var newPaper = new joint.dia.Paper({
       el: paperElement,
       model: graph,
@@ -17,17 +17,17 @@ export class Paper {
       drawGrid: { name: 'mesh', args: { color: 'black' } },
       background: { color: 'white'},
       cellViewNamespace: nameSpace,
-      interactive: true, // povoluje interakciu medzi elementami
-      linkPinning: false, // zakaze pouzivatelovi pinnut link na papier
+      interactive: true, // Element interaction on paper
+      linkPinning: false, // Forbids link pinning on paper
       multiLinks: false,
-      defaultLink: new joint.shapes.standard.Link(),
+      defaultLink: new joint.shapes.standard.Link({router: {name: 'manhattan'}}),
       defaultConnector: { name: 'rounded' },
       defaultConnectionPoint: { name: 'boundary' },
       defaultAnchor: { name: 'perpendicular', args: { padding: 10 } },
       defaultLinkAnchor: { name: 'perpendicular', args: { padding: 10 } },
       defaultRouter: { name: 'normal' },
       validateConnection: function(cellViewS, magnetS, cellViewT, magnetT, end, linkView) {
-        // zakaz spojenie so samym sebou
+        // Prevent from linking with it self
         if(cellViewS === cellViewT) {
           return false; 
         }
