dfa626e942b84d43a7a6f9bff4fadf4e6bd77e7b
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-05-07 22:34:54 +0200

    Added/fixed some styling for toolbar

--- src/app/routing/activity-diagrams-routing.module.ts
+++ src/app/routing/activity-diagrams-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { MenuComponent } from "../activity-diagrams/menu/menu.component";
 
 const routes: Routes = [
     {
         path: "", children: [
-            { path: "", component: MenuComponent }
         ]
     }
 ];
